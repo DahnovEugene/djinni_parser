c3bf2d560dd5c4e8f8472a35feb095da771d11af
DahnovEugene <dahnov.work@gmail.net>
2022-12-26 22:52:07 +0200

    Fixing a paging error Added possibility to choose work experience

--- pars.py
+++ pars.py
@@ -3,33 +3,43 @@ from bs4 import BeautifulSoup
 import time
 
 
-def get_data():
+def get_data(xp_level: str = 'no_exp'):
     list_skill = []
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0'
     }
-    url_djinni = "https://djinni.co/jobs/?primary_keyword=Python&region=UKR"
-    response = requests.get(url_djinni, headers=headers)
-    soap = BeautifulSoup(response.text, 'lxml')
-    div_title = soap.find_all('div', 'list-jobs__title')
-
+    url_djinni = f"https://djinni.co/jobs/?primary_keyword=Python&region=UKR&exp_level={xp_level}"
+    count_page = 1
     n = 1
-    for page in range(1):
-        link = url_djinni + f'&page={n}'
-        req = requests.get(link, headers=headers)
-        if 'Вакансії Python' in req.text.title():
-            print(f'&page={n}')
-            n += 1
-        else:
-            break
-        time.sleep(3)
+    title_list = []
+    find_number_page = requests.get(url_djinni, headers=headers)
+    soap_page = BeautifulSoup(find_number_page.text, 'lxml')
+    li = soap_page.find_all('li', 'page-item')
+    lst = []
+    for i in li:
+        el = i.text.strip()
+        if el != '':
+            lst.append(el)
+    if len(lst) > 0:
+        n = int(lst[-1])
+    elif len(lst) <= 0:
+        n = 1
+
+    for page in range(n):
+        link = url_djinni + f'&page={count_page}'
+        print(f'page={count_page}')
+        response = requests.get(link, headers=headers)
+        soap = BeautifulSoup(response.text, 'lxml')
+        div_title = soap.find_all('div', 'list-jobs__title')
+        time.sleep(2)
+        count_page += 1
 
         for div in div_title:
             link = div.a.get('href')
             title = div.span.text
             page_link = 'https://djinni.co' + link
 
-            req = requests.get(page_link)
+            req = requests.get(page_link, headers=headers)
             soap = BeautifulSoup(req.text, 'lxml')
             toolbar = soap.find_all('div', 'job-additional-info--item-text')
             skills = list(toolbar)[1].text.strip().split(',')
@@ -37,7 +47,9 @@ def get_data():
             for el in skills:
                 new_el = el.replace('\n', ' ').strip().lower()
                 list_skill.append(new_el)
-    print(list_skill)
+            title_list.append(title)
+    print(len(title_list))
+    print(len(list_skill))
 
 
 def main():
