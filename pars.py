22364741cb767d5255dbb282c2eb45183fccc7c7
DahnovEugene <dahnov.work@gmail.com>
2023-03-13 17:23:08 +0200

    In pars.py : add id_work,
    the file recording function has been updated,
    
    In job_bot.py:
    func get_data replaced on writer_file

--- pars.py
+++ pars.py
@@ -2,7 +2,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 import time
-from settings import params, cookies, headers
+from settings import params, cookies, headers, months
 
 
 def get_data(xp_level: str):
@@ -10,15 +10,17 @@ def get_data(xp_level: str):
     simple_input = ['no_exp', '1y', '2y', '3y', '4y', '5y']
     at the output list
     """
-    jobs = []
-    id_work = 0
+    try:
+        vacancies = read_file()
+    except FileNotFoundError:
+        vacancies = []
 
     url_djinni = f"https://djinni.co/jobs/?primary_keyword=Python&region=UKR&exp_level={xp_level}"
     count_page = 1
 
     s = requests.Session()
-    response = s.get(url=url_djinni, params=params, cookies=cookies, headers=headers).text
-    soup = BeautifulSoup(response, 'lxml')
+    response = s.get(url=url_djinni, params=params, cookies=cookies, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
 
     # find last page
 
@@ -42,14 +44,23 @@ def get_data(xp_level: str):
         # find all links job on page
 
         for div in div_title:
-            id_work += 1
             link = div.a.get('href')
             title = div.span.text
             page_link = 'https://djinni.co' + link
 
-            req = s.get(page_link, params=params, cookies=cookies, headers=headers)
-            soap = BeautifulSoup(req.text, 'lxml')
+            response = s.get(page_link, params=params, cookies=cookies, headers=headers)
+            soap = BeautifulSoup(response.text, 'lxml')
             toolbar = soap.find_all('li', class_="job-additional-info--item")
+
+            # get time created
+            get_time_created = soap.find('div', class_="text-small")
+            lst_time = []
+            for i in get_time_created:
+                p = i.text.replace('\n', '').strip().split(' ')
+                for el in p:
+                    if el != '':
+                        lst_time.append(el)
+            time_created = lst_time[2] + '/' + months[lst_time[3]] + '/' + lst_time[4]
 
             #  collection of all requirements to the list
 
@@ -64,23 +75,39 @@ def get_data(xp_level: str):
                     temp_list.append(requirement)
                 requirements.append(temp_list)
 
-            jobs_dict['id_work'] = id_work
+            jobs_dict['xp_level'] = xp_level
             jobs_dict['title'] = title
             jobs_dict['link'] = page_link
             jobs_dict['requirements'] = requirements
-            jobs.append(jobs_dict)
+            jobs_dict['time_created'] = time_created
 
-    return jobs
+            if jobs_dict not in vacancies:
+                vacancies.append(jobs_dict)
+
+    return vacancies
 
 
 def writer_file(xp_level: str):
     data = get_data(xp_level)
+    id_work = 0
+    for el in data:
+        id_work += 1
+        el['id_work'] = id_work
     with open('some_json.json', 'w') as f:
         json.dump(data, f, indent=4)
+        print('Finish write!')
+
+
+def read_file():
+    with open('some_json.json') as f:
+        data = json.load(f)
+    return data
 
 
 def main():
-    get_data('1y')
+    writer_file('no_exp')
+    # print(get_data('1y'))
+    # print(get_data('1y'))
 
 
 if __name__ == '__main__':
